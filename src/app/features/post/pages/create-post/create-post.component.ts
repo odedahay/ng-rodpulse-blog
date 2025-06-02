327b7d3349b3cf265c8a90303a901f9541b1bde1
odedahay <odedahay@yahoo.com>
2025-06-01 18:03:11 -0600

    Add Tailwind CSS integration and update Angular configuration
    - Updated `angular.json` to include assets from `src/assets`.
    - Added Tailwind CSS and PostCSS dependencies in `package.json` and `package-lock.json`.
    - Imported Tailwind CSS in `src/styles.css`.
    - Refactored `app.component.html` to include a new `<app-navbar>` component.
    - Updated routing in `app.routes.ts` to include a new route for creating posts.

---
+++ src/app/features/post/pages/create-post/create-post.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+
+@Component({
+  selector: 'app-create-post',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './create-post.component.html',
+  styleUrl: './create-post.component.css'
+})
+export class CreatePostComponent {
+  createPostForm = new FormGroup({
+    title: new FormControl<string>('',
+      {
+        nonNullable: true,
+        validators: [Validators.required, Validators.maxLength(100), Validators.minLength(6)]
+      }
+    ),
+    content: new FormControl<string>('',
+      {
+        nonNullable: true,
+        validators: [Validators.required, Validators.maxLength(3000)]
+      }
+    )
+  });
+
+  get title(){
+    return this.createPostForm.controls.title;
+  }
+
+  get content(){
+    return this.createPostForm.controls.content;
+  }
+
+  onFormSubmit() {
+    console.log(this.createPostForm.value)
+  }
+}
