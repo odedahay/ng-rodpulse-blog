23bd8208e01a551c2208306d666d3f3565a4111d
odedahay <odedahay@yahoo.com>
2025-06-15 05:58:17 -0600

    Refactor ViewPostComponent to improve loading state and error handling
    - Updated the HTML template to display a loading message while fetching blog post data.
    - Enhanced the component logic to utilize `rxResource` for managing blog post retrieval and loading state.
    - Added error handling to display a message when a blog post is not found.
    - Removed unused code related to slug observable for cleaner implementation.
    - Updated environment configuration with hardcoded Firebase credentials for development.

--- src/app/features/post/pages/view-post/view-post.component.ts
+++ src/app/features/post/pages/view-post/view-post.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, input } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
+import { rxResource, toObservable } from '@angular/core/rxjs-interop';
 import { BlogpostService } from '../../services/blogpost.service';
 import { filter, switchMap } from 'rxjs';
 import { AsyncPipe, DatePipe } from '@angular/common';
@@ -8,7 +8,7 @@ import { MarkdownComponent, MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'app-view-post',
-  imports: [AsyncPipe, DatePipe, MarkdownModule, MarkdownComponent],
+  imports: [DatePipe, MarkdownModule, MarkdownComponent],
   templateUrl: './view-post.component.html',
   styleUrl: './view-post.component.css'
 })
@@ -17,10 +17,21 @@ export class ViewPostComponent {
   converTimestampToDate = BlogPostHelper.converTimestampToDate;
 
   slug = input<string | undefined>(undefined);
-  slug$ = toObservable(this.slug);
+  // slug$ = toObservable(this.slug);
 
-  blogPost$ = this.slug$.pipe(
-    filter(slug => slug !== undefined),
-    switchMap(slug => this.blogPostService.getBlogPostBySlug(slug))
-  );
+  // blogPost$ = this.slug$.pipe(
+  //   filter(slug => slug !== undefined),
+  //   switchMap(slug => this.blogPostService.getBlogPostBySlug(slug))
+  // );
+
+  // New Method RxResource
+
+  private blogPostResource = rxResource({
+    request: ()=> this.slug(),
+    loader:({ request: slug }) => this.blogPostService.getBlogPostBySlug(slug)
+  })
+
+  blogPostData = this.blogPostResource.value;
+  isLoading = this.blogPostResource.isLoading;
+
 }
